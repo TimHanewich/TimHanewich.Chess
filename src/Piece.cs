21c02151e23b3afddc5ca6494b57efbd3810e852
Tim Hanewich <tahanewich@live.com>
2021-12-09 18:52:18 -0500

    Added Piece

---
+++ src/Piece.cs
@@ -0,0 +1,45 @@
+using System;
+
+namespace TimHanewich.Chess
+{
+    public class Piece
+    {
+        public PieceType Type {get; set;}
+        public Position Position {get; set;}
+        
+        public float Value
+        {
+            get
+            {
+                if (Type == PieceType.Pawn)
+                {
+                    return 1f;
+                }
+                else if (Type == PieceType.Knight)
+                {
+                    return 3f;
+                }
+                else if (Type == PieceType.Bishop)
+                {
+                    return 3f;
+                }
+                else if (Type == PieceType.Rook)
+                {
+                    return 5f;
+                }
+                else if (Type == PieceType.Queen)
+                {
+                    return 10f;
+                }
+                else if (Type == PieceType.King)
+                {
+                    return 15f;
+                }
+                else
+                {
+                    return 0f;
+                }
+            }
+        }
+    }
+}
