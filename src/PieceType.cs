56e71386c036fac7c9fc332e13b49bdbfc7ce8af
Tim Hanewich <tahanewich@live.com>
2021-12-09 18:48:03 -0500

    Added PieceType

---
+++ src/PieceType.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace TimHanewich.Chess
+{
+    public enum PieceType
+    {
+        Pawn = 0,
+        Knight = 1,
+        Bishop = 2,
+        Rook = 3,
+        Queen = 4,
+        King = 5
+    }
+}
