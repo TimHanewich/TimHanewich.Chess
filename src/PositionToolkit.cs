1a4dad4e7bc32f08524dbc80b12026f9df9f41d0
Tim Hanewich <tahanewich@live.com>
2021-12-10 21:56:23 -0500

    Rewrote left (optimization)

--- src/PositionToolkit.cs
+++ src/PositionToolkit.cs
@@ -652,51 +652,128 @@ namespace TimHanewich.Chess
                 case Position.G1:
                     return Position.H1;
                 default:
-                    throw new Exception("Maximum rank achieved.");
+                    throw new Exception("Maximum rank already achieved.");
             }
         }
 
         public static Position Left(this Position position)
         {
-            char CurrentFile = position.File();
-            if (CurrentFile == 'A')
+            switch (position)
             {
-                throw new Exception("Minumum file already achieved.");
-            }
-            else
-            {
-                char NewFile = 'H';
-                if (CurrentFile == 'H')
-                {
-                    NewFile = 'G';
-                }
-                else if (CurrentFile == 'G')
-                {
-                    NewFile = 'F';
-                }
-                else if (CurrentFile == 'F')
-                {
-                    NewFile = 'E';
-                }
-                else if (CurrentFile == 'E')
-                {
-                    NewFile = 'D';
-                }
-                else if (CurrentFile == 'D')
-                {
-                    NewFile = 'C';
-                }
-                else if (CurrentFile == 'C')
-                {
-                    NewFile = 'B';
-                }
-                else if (CurrentFile == 'B')
-                {
-                    NewFile = 'A';
-                }
-                
-                Position ToReturn = PositionToolkit.Parse(NewFile.ToString() + position.Rank());
-                return ToReturn;
+                case Position.H8:
+                    return Position.G8;
+                case Position.G8:
+                    return Position.F8;
+                case Position.F8:
+                    return Position.E8;
+                case Position.E8:
+                    return Position.D8;
+                case Position.D8:
+                    return Position.C8;
+                case Position.C8:
+                    return Position.B8;
+                case Position.B8:
+                    return Position.A8;
+                case Position.H7:
+                    return Position.G7;
+                case Position.G7:
+                    return Position.F7;
+                case Position.F7:
+                    return Position.E7;
+                case Position.E7:
+                    return Position.D7;
+                case Position.D7:
+                    return Position.C7;
+                case Position.C7:
+                    return Position.B7;
+                case Position.B7:
+                    return Position.A7;
+                case Position.H6:
+                    return Position.G6;
+                case Position.G6:
+                    return Position.F6;
+                case Position.F6:
+                    return Position.E6;
+                case Position.E6:
+                    return Position.D6;
+                case Position.D6:
+                    return Position.C6;
+                case Position.C6:
+                    return Position.B6;
+                case Position.B6:
+                    return Position.A6;
+                case Position.H5:
+                    return Position.G5;
+                case Position.G5:
+                    return Position.F5;
+                case Position.F5:
+                    return Position.E5;
+                case Position.E5:
+                    return Position.D5;
+                case Position.D5:
+                    return Position.C5;
+                case Position.C5:
+                    return Position.B5;
+                case Position.B5:
+                    return Position.A5;
+                case Position.H4:
+                    return Position.G4;
+                case Position.G4:
+                    return Position.F4;
+                case Position.F4:
+                    return Position.E4;
+                case Position.E4:
+                    return Position.D4;
+                case Position.D4:
+                    return Position.C4;
+                case Position.C4:
+                    return Position.B4;
+                case Position.B4:
+                    return Position.A4;
+                case Position.H3:
+                    return Position.G3;
+                case Position.G3:
+                    return Position.F3;
+                case Position.F3:
+                    return Position.E3;
+                case Position.E3:
+                    return Position.D3;
+                case Position.D3:
+                    return Position.C3;
+                case Position.C3:
+                    return Position.B3;
+                case Position.B3:
+                    return Position.A3;
+                case Position.H2:
+                    return Position.G2;
+                case Position.G2:
+                    return Position.F2;
+                case Position.F2:
+                    return Position.E2;
+                case Position.E2:
+                    return Position.D2;
+                case Position.D2:
+                    return Position.C2;
+                case Position.C2:
+                    return Position.B2;
+                case Position.B2:
+                    return Position.A2;
+                case Position.H1:
+                    return Position.G1;
+                case Position.G1:
+                    return Position.F1;
+                case Position.F1:
+                    return Position.E1;
+                case Position.E1:
+                    return Position.D1;
+                case Position.D1:
+                    return Position.C1;
+                case Position.C1:
+                    return Position.B1;
+                case Position.B1:
+                    return Position.A1;
+                default:
+                    throw new Exception("Minumum file already achieved.");
             }
         }
 
