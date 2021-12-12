371da6f07bd78d08388cb768471a9727db5e5ca6
Tim Hanewich <tahanewich@live.com>
2021-12-12 12:55:38 -0500

    Move notation

--- src/Move.cs
+++ src/Move.cs
@@ -43,5 +43,81 @@ namespace TimHanewich.Chess
                 }
             }
         }
+    
+        public string ToAlgebraicNotation(BoardPosition position)
+        {
+            Piece MovingPiece = position.FindOccupyingPiece(FromPosition);
+            if (MovingPiece == null)
+            {
+                throw new Exception("Unable to convert move to algebraic notation. Piece not found on from position square.");
+            }
+
+            //Get the beginning piece notation
+            string PieceNotation = "";
+            if (MovingPiece.Type == PieceType.Rook)
+            {
+                PieceNotation = "R";
+            }
+            else if (MovingPiece.Type ==  PieceType.King)
+            {
+                PieceNotation = "K";
+            }
+            else if (MovingPiece.Type == PieceType.Queen)
+            {
+                PieceNotation = "Q";
+            }
+            else if (MovingPiece.Type == PieceType.Bishop)
+            {
+                PieceNotation = "B";
+            }
+            else if (MovingPiece.Type == PieceType.Knight)
+            {
+                PieceNotation = "N";
+            }
+
+            //Is this a take? Is it a capture of an enemy piece?
+            string CaptureNotation = "";
+            Piece CapturingPiece = position.FindOccupyingPiece(ToPosition);
+            if (CapturingPiece != null)
+            {
+                if (CapturingPiece.Color != MovingPiece.Color)
+                {
+                    CaptureNotation = "x";
+                }
+            }
+
+            //Disambiguating move?
+            string DisambiguatingNotation = "";
+            //Check if there are any other pieces of the same type that can also move to the destination square
+            foreach (Piece p in position.Pieces)
+            {
+                if (p.Position != FromPosition)
+                {
+                    if (p.Color == MovingPiece.Color)
+                    {
+                        if (p.Type == MovingPiece.Type)
+                        {
+                            //What is the difference? Is it rank or is it file?
+                            if (p.Position.Rank() == MovingPiece.Position.Rank())
+                            {
+                                DisambiguatingNotation = MovingPiece.Position.File().ToString().ToLower();
+                            }
+                            else if (p.Position.File() == MovingPiece.Position.File())
+                            {
+                                DisambiguatingNotation = MovingPiece.Position.Rank().ToString();
+                            }
+                        }
+                    }
+                }
+            }
+
+
+            //Get the Position part
+            string PositionNotation = ToPosition.ToString().ToLower();
+
+            //String it all together and return
+            string ToReturn = PieceNotation + CaptureNotation + DisambiguatingNotation + PositionNotation;
+            return ToReturn;
+        }
     }
 }
