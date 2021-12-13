9d2906a7dbe9baa782cbbab59574710bbfe82e28
Tim Hanewich <tahanewich@live.com>
2021-12-13 09:23:24 -0700

    Renamed to BoardState

--- src/Experimental/BoardPosition.cs
+++ src/Experimental/BoardPosition.cs
@@ -3,14 +3,9 @@ using System.Collections.Generic;
 
 namespace TimHanewich.Chess.Experimental
 {
-    public class BoardPosition
+    public class BoardState
     {
         private int[] BoardStructure;
         public bool WhiteToMove {get; set;}
-
-        public BoardPosition[] AvailableMoves()
-        {
-            
-        }
     }
 }
