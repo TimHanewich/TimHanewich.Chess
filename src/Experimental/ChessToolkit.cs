c542d52304c0fc2ba1fd0f43ce6af22eb5e34edc
Tim Hanewich <tahanewich@live.com>
2021-12-12 19:02:28 -0500

    Up

--- src/Experimental/ChessToolkit.cs
+++ src/Experimental/ChessToolkit.cs
@@ -20,6 +20,134 @@ namespace TimHanewich.Chess.Experimental
         //11 = Black knight
         //12 = Black pawn
 
+        #region "Movemment"
+
+        //These return the index of the position in the byte array relative to the starting position (in byte format)
+
+        public static byte Up(this byte array_position)
+        {
+            switch (array_position)
+            {
+                case 0:
+                    return 1;   
+                case 1:
+                    return 2;  
+                case 2:
+                    return 3;  
+                case 3:
+                    return 4;  
+                case 4:
+                    return 5;  
+                case 5:
+                    return 6;  
+                case 6:
+                    return 7;  
+                case 8:
+                    return 9;
+                case 9:
+                    return 10; 
+                case 10:
+                    return 11;  
+                case 11:
+                    return 12;  
+                case 12:
+                    return 13;  
+                case 13:
+                    return 14;  
+                case 14:
+                    return 15; 
+                case 16: //File C
+                    return 17; 
+                case 17:
+                    return 18;
+                case 18:
+                    return 19; 
+                case 19:
+                    return 20; 
+                case 20:
+                    return 21; 
+                case 21:
+                    return 22; 
+                case 22:
+                    return 23;  
+                case 24: //File D
+                    return 25; 
+                case 25:
+                    return 26;
+                case 26:
+                    return 27; 
+                case 27:
+                    return 28; 
+                case 28:
+                    return 29; 
+                case 29:
+                    return 30; 
+                case 30:
+                    return 31;  
+                case 32: //File E
+                    return 33; 
+                case 33:
+                    return 34;
+                case 34:
+                    return 35; 
+                case 35:
+                    return 36; 
+                case 36:
+                    return 37; 
+                case 37:
+                    return 38; 
+                case 38:
+                    return 39;  
+                case 40: //File F
+                    return 41; 
+                case 41:
+                    return 42;
+                case 42:
+                    return 43; 
+                case 43:
+                    return 44; 
+                case 44:
+                    return 45; 
+                case 45:
+                    return 46; 
+                case 46:
+                    return 47;   
+                case 48: //File G
+                    return 49; 
+                case 49:
+                    return 50;
+                case 50:
+                    return 51; 
+                case 51:
+                    return 52; 
+                case 52:
+                    return 53; 
+                case 53:
+                    return 54; 
+                case 54:
+                    return 55; 
+                case 56: //File H
+                    return 57; 
+                case 57:
+                    return 58;
+                case 58:
+                    return 59; 
+                case 59:
+                    return 60; 
+                case 60:
+                    return 61; 
+                case 61:
+                    return 62; 
+                case 62:
+                    return 63; 
+                default:
+                    throw new Exception("It is impossible to move up from array position " + array_position.ToString());                               
+                
+            }
+        }
+
+        #endregion
+
 
         public static byte ToByte(this Piece p)
         {
