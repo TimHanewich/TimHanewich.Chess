212a344fcaf5fb7b136ddcc065c3e9a4ed16f318
Tim Hanewich <tahanewich@live.com>
2021-12-16 09:37:15 -0500

    Set up test

---
+++ test/PgnTest.cs
@@ -0,0 +1,18 @@
+using System;
+using TimHanewich.Chess;
+using TimHanewich.Chess.PGN;
+using Xunit;
+
+namespace test
+{
+    public class PgnTest
+    {
+        [Fact]
+        public void PgnParsingTest()
+        {
+            string content = System.IO.File.ReadAllText("../../../game1.pgn");
+            PgnParserLite pgn = PgnParserLite.ParsePgn(content);
+            Assert.True(true);
+        }
+    }
+}
