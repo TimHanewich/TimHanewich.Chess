049a1de0ca9d924a76fa79eeb6ac139862a0669a
Tim Hanewich <tahanewich@live.com>
2022-01-15 12:31:18 -0500

    Moved MassivePgnFileSplitter over from TimHanewich.Chess.BatchAnalysis nuget package

---
+++ src/PGN/MassivePgnFileSplitter.cs
@@ -0,0 +1,40 @@
+using System;
+using System.IO;
+
+namespace TimHanewich.Chess.PGN
+{
+    public class MassivePgnFileSplitter
+    {
+        private StreamReader SR;
+        private string Buff;
+
+        public MassivePgnFileSplitter(Stream s)
+        {
+            SR = new StreamReader(s);
+            Buff = SR.ReadLine();
+        }
+
+        public string NextGame()
+        {
+            string NextGame = "";
+            NextGame = Buff + Environment.NewLine;
+            bool KillNow = false;
+            do
+            {
+                string next_line = SR.ReadLine();
+
+                if (next_line.Contains("[Event "))
+                {
+                    KillNow = true;
+                    Buff = next_line;
+                }
+                else
+                {
+                    NextGame = NextGame + next_line + Environment.NewLine;
+                }
+            } while (KillNow == false);
+
+            return NextGame.Trim();
+        }
+    }
+}
