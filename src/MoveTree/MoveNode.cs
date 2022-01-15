a3b9f354ec466254497a86a91c3857c62c0aed4d
Tim Hanewich <tahanewich@live.com>
2022-01-15 13:04:22 -0500

    Added MoveNode constructors

--- src/MoveTree/MoveNode.cs
+++ src/MoveTree/MoveNode.cs
@@ -7,5 +7,26 @@ namespace TimHanewich.Chess.MoveTree
         public string Move {get; set;}
         public uint Occurances {get; set;}
         public MoveNode[] ChildNodes {get; set;}
+
+        #region "Constructors"
+
+        private void Initialize()
+        {
+            ChildNodes = new MoveNode[]{};
+        }
+
+        public MoveNode()
+        {
+            Initialize();
+        }
+
+        public MoveNode(string move)
+        {
+            Initialize();
+            Move = move;
+        }
+
+        #endregion
+
     }
 }
