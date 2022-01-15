eb29ec4545370839cd8ed8e4b2835b5be578b494
Tim Hanewich <tahanewich@live.com>
2022-01-15 13:41:44 -0500

    Constructor

--- src/MoveTree/MoveNodeTree.cs
+++ src/MoveTree/MoveNodeTree.cs
@@ -5,28 +5,12 @@ namespace TimHanewich.Chess.MoveTree
     public class MoveNodeTree
     {
         //The move nodes
-        private MoveNode RootNode; //This is just a place holder that is used to organize the first moves that can be made by white. The child move nodes of this move node are white's potential first moves. This is not surfaced to the user.
-        public MoveNode[] MoveNodes
-        {
-            get
-            {
-                return RootNode.ChildNodes;
-            }
-        }
-    
-        //Adding a new first move
-        public void AddFirstMoveNode(MoveNode node)
-        {
-            try
-            {
-                RootNode.AddChildNode(node);
-            }
-            catch (Exception ex)
-            {
-                throw new Exception("Unable to add first move node. Msg: " + ex.Message);
-            }
-        }
+        public MoveNode GameStart {get; set;}
 
+        public MoveNodeTree()
+        {
+            GameStart = new MoveNode();
+        }
     }
 
 }
--- src/MoveTree/MoveNodeTreeConstructor.cs
+++ src/MoveTree/MoveNodeTreeConstructor.cs
@@ -7,6 +7,7 @@ namespace TimHanewich.Chess.MoveTree
     public class MoveNodeTreeConstructor
     {
         private Stream MassivePgnFileStream;
+        private MassivePgnFileSplitter Splitter;
 
         //Progress indicators
         public uint GamesAdded;
@@ -23,12 +24,53 @@ namespace TimHanewich.Chess.MoveTree
 
         #endregion
 
-        public void Construct(int number_of_games)
+        public void ContinueConstruction(int number_of_games)
         {
+            //If the stream has not been set, throw an error
+            if (MassivePgnFileStream == null)
+            {
+                throw new Exception("Unable to construct move node tree: Massive PGN file input stream was not set.");
+            }
+
             //Create the resulting move node tree if it doesnt exist
             if (ResultingMoveNodeTree == null)
             {
                 ResultingMoveNodeTree = new MoveNodeTree();
+            }
+
+            //Make the splitter if it doesn't exist
+            if (Splitter == null)
+            {
+                Splitter = new MassivePgnFileSplitter(MassivePgnFileStream);
+            }
+
+            //Do the # of games they asked for.
+            for (int t = 0; t < number_of_games; t++)
+            {
+                string ThisGame = Splitter.NextGame();
+                if (ThisGame != null)
+                {
+                    //Parse this PGN
+                    PgnFile pgn = PgnFile.ParsePgn(ThisGame);
+                    MoveNode OnNode = ResultingMoveNodeTree.GameStart;
+                    foreach (string move in pgn.Moves)
+                    {
+                        MoveNode MoveThatWasMade = OnNode.FindChildNode(move);
+                        if (MoveThatWasMade == null) //Our tree has not seen this first move yet
+                        {
+                            MoveNode ThisNewMove = new MoveNode();
+                            ThisNewMove.Move = move;
+                            ThisNewMove.Occurances = 1;
+                            OnNode.AddChildNode(ThisNewMove);
+                            OnNode = ThisNewMove;
+                        }
+                        else //We have seen this move before. So increment the # of times we have seen it, and then set this as the node we are on now
+                        {
+                            MoveThatWasMade.Occurances = MoveThatWasMade.Occurances + 1;
+                            OnNode = MoveThatWasMade;
+                        }
+                    }
+                }
             }
         }
 
