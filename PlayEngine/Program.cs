5ed958a4069de0994883751dbf4cfcf5dd95f829
Tim Hanewich <tahanewich@live.com>
2022-01-15 14:35:03 -0500

    Move over play engine from testing

--- PlayEngine/Program.cs
+++ PlayEngine/Program.cs
@@ -6,7 +6,83 @@ namespace PlayEngine
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Original position? FEN>");
+            string BeginningFen = Console.ReadLine();
+            BoardPosition PlayBoard = new BoardPosition(BeginningFen);
+            Console.Write("Depth? >");
+            int depth = Convert.ToInt32(Console.ReadLine());
+            
+            EvaluationEngine ee = new EvaluationEngine();
+            ee.EvaluationStatusUpdated += PrintStatus;
+            //ee.EvaluationProgressUpdated += PrintPercentComplete;
+            ee.EvaluationEstimatedTimeRemainingUpdated += PrintTimeRemaining;
+
+            //Create a history of moves in positions
+            MoveDecisionHistory mdh = new MoveDecisionHistory();
+            
+            while (true)
+            {
+                //Make our move.
+                Console.WriteLine("Finding moves...");
+                MoveAssessment[] moves = ee.FindBestMoves(PlayBoard, depth);
+                if (moves.Length == 0)
+                {
+                    Console.WriteLine("I have no moves to play! I resign.");
+                }
+
+                //Print all potential moves
+                foreach (MoveAssessment ma in moves)
+                {
+                    Console.WriteLine(ma.Move.ToAlgebraicNotation(PlayBoard) + " = " + ma.ResultingEvaluation.ToString());
+                }
+
+                //Select the move to make
+                Move ToMake = null;
+                Move MoveMadePreviouslyInThisPosition = mdh.Find(PlayBoard.BoardRepresentation());
+                if (MoveMadePreviouslyInThisPosition == null)
+                {
+                    ToMake = moves[0].Move;
+                }
+                else //We have been in this move previously in this exact position. Try to find a different move
+                {
+                    foreach (MoveAssessment ma in moves)
+                    {
+                        if (ma.Move.ToPosition != MoveMadePreviouslyInThisPosition.ToPosition) //This isn't the move we made last time
+                        {
+                            ToMake = ma.Move;
+                            break;
+                        }
+                    }
+                }
+                if (ToMake == null)
+                {
+                    ToMake = moves[0].Move;
+                }
+
+                //Record the move we are going to make (have selected)
+                mdh.Add(PlayBoard.BoardRepresentation(), ToMake);
+
+                //Print and make the best move to make
+                string AsNotation = ToMake.ToAlgebraicNotation(PlayBoard);
+                PlayBoard.ExecuteMove(ToMake); //Execute move
+                mdh.Add(PlayBoard.BoardRepresentation(), ToMake);
+                Console.WriteLine("I play " + AsNotation + " (" + ToMake.FromPosition.ToString() + " --> " + ToMake.ToPosition.ToString() + ")");
+
+                //Make their move
+                Console.WriteLine();
+                Console.WriteLine("What move do you play?");
+                Console.Write("From: ");
+                Position From = PositionToolkit.Parse(Console.ReadLine());
+                Console.Write("To: ");
+                Position To = PositionToolkit.Parse(Console.ReadLine());
+                Move m = new Move();
+                m.FromPosition = From;
+                m.ToPosition = To;
+                Console.Write("Executing move... ");
+                PlayBoard.ExecuteMove(m);
+                Console.WriteLine("Executed.");
+                Console.WriteLine();
+            }
         }
     }
 }
--- testing/Program.cs
+++ testing/Program.cs
@@ -4,6 +4,8 @@ using TimHanewich.Chess.PGN;
 using Newtonsoft.Json;
 using TimHanewich.Toolkit;
 using System.Collections.Generic;
+using System.IO;
+using TimHanewich.Chess.MoveTree;
 
 namespace testing
 {
@@ -12,8 +14,21 @@ namespace testing
         static void Main(string[] args)
         {
             
+            Stream s = System.IO.File.OpenRead(@"C:\Users\tahan\Downloads\lichess_db_standard_rated_2016-07.pgn");
             
-            PlayEngine();
+            MoveNodeTreeConstructor con = new MoveNodeTreeConstructor(s);
+            con.ContinueConstruction(5000, 15);
+            MoveNode pop = con.ResultingMoveNodeTree.GameStart.MostPopularChildNode();
+            while (true)
+            {
+                Console.WriteLine(pop.Move + " " + pop.ResultedInWhiteVictory.ToString() + " " + pop.ResultedInDraw.ToString() + " " + pop.ResultedInBlackVictory.ToString());
+                pop = pop.MostPopularChildNode();
+                if (pop == null)
+                {
+                    Console.WriteLine("Thats it!");
+                    return;
+                }
+            }
 
             
         }
@@ -42,87 +57,6 @@ namespace testing
             float eval = bp.Evaluate(7);
             ht.StopTimer();
             Console.WriteLine(" eval " + eval.ToString() + " in " + ht.GetElapsedTime().TotalSeconds.ToString() + " seconds");
-        }
-
-        public static void PlayEngine()
-        {
-            Console.Write("Original position? FEN>");
-            string BeginningFen = Console.ReadLine();
-            BoardPosition PlayBoard = new BoardPosition(BeginningFen);
-            Console.Write("Depth? >");
-            int depth = Convert.ToInt32(Console.ReadLine());
-            
-            EvaluationEngine ee = new EvaluationEngine();
-            ee.EvaluationStatusUpdated += PrintStatus;
-            //ee.EvaluationProgressUpdated += PrintPercentComplete;
-            ee.EvaluationEstimatedTimeRemainingUpdated += PrintTimeRemaining;
-
-            //Create a history of moves in positions
-            MoveDecisionHistory mdh = new MoveDecisionHistory();
-            
-            while (true)
-            {
-                //Make our move.
-                Console.WriteLine("Finding moves...");
-                MoveAssessment[] moves = ee.FindBestMoves(PlayBoard, depth);
-                if (moves.Length == 0)
-                {
-                    Console.WriteLine("I have no moves to play! I resign.");
-                }
-
-                //Print all potential moves
-                foreach (MoveAssessment ma in moves)
-                {
-                    Console.WriteLine(ma.Move.ToAlgebraicNotation(PlayBoard) + " = " + ma.ResultingEvaluation.ToString());
-                }
-
-                //Select the move to make
-                Move ToMake = null;
-                Move MoveMadePreviouslyInThisPosition = mdh.Find(PlayBoard.BoardRepresentation());
-                if (MoveMadePreviouslyInThisPosition == null)
-                {
-                    ToMake = moves[0].Move;
-                }
-                else //We have been in this move previously in this exact position. Try to find a different move
-                {
-                    foreach (MoveAssessment ma in moves)
-                    {
-                        if (ma.Move.ToPosition != MoveMadePreviouslyInThisPosition.ToPosition) //This isn't the move we made last time
-                        {
-                            ToMake = ma.Move;
-                            break;
-                        }
-                    }
-                }
-                if (ToMake == null)
-                {
-                    ToMake = moves[0].Move;
-                }
-
-                //Record the move we are going to make (have selected)
-                mdh.Add(PlayBoard.BoardRepresentation(), ToMake);
-
-                //Print and make the best move to make
-                string AsNotation = ToMake.ToAlgebraicNotation(PlayBoard);
-                PlayBoard.ExecuteMove(ToMake); //Execute move
-                mdh.Add(PlayBoard.BoardRepresentation(), ToMake);
-                Console.WriteLine("I play " + AsNotation + " (" + ToMake.FromPosition.ToString() + " --> " + ToMake.ToPosition.ToString() + ")");
-
-                //Make their move
-                Console.WriteLine();
-                Console.WriteLine("What move do you play?");
-                Console.Write("From: ");
-                Position From = PositionToolkit.Parse(Console.ReadLine());
-                Console.Write("To: ");
-                Position To = PositionToolkit.Parse(Console.ReadLine());
-                Move m = new Move();
-                m.FromPosition = From;
-                m.ToPosition = To;
-                Console.Write("Executing move... ");
-                PlayBoard.ExecuteMove(m);
-                Console.WriteLine("Executed.");
-                Console.WriteLine();
-            }
         }
     }
 }
