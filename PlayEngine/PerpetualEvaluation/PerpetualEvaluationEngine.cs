9427d90b3509bfa118e00624de45fb34ede655ed
Tim Hanewich <tahanewich@live.com>
2022-01-22 12:05:02 -0500

    Dumping

--- PlayEngine/PerpetualEvaluation/PerpetualEvaluationEngine.cs
+++ PlayEngine/PerpetualEvaluation/PerpetualEvaluationEngine.cs
@@ -1,6 +1,7 @@
 using System;
 using TimHanewich.Chess;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 
 
 namespace PlayEngine.PerpetualEvaluation
@@ -51,6 +52,7 @@ namespace PlayEngine.PerpetualEvaluation
                 UpdateStatus("Move executed!");
             }
             UpdateStatus("Stopped!");
+            StopConfirmed = true; //Signal that the stop is indeed successful.
         }
 
         //Stopping
@@ -70,6 +72,25 @@ namespace PlayEngine.PerpetualEvaluation
             EvaluateFromPosition = root;
         }
 
+
+        #region "Dumping of TranspositionTable"
+
+        public void DumpTranspositionTable(TranspositionTable dump_to)
+        {
+            //If the process is currently running, throw an error
+            if (StopPerpetuallyEvaluating == false)
+            {
+                throw new Exception("Unable to dump local transposition table buffer while perpetual evaluation is running. First, stop the perpetual evaluation with the StopAsync method.");
+            }
+            
+            //Dump
+            foreach (KeyValuePair<int[], EvaluationPackage> kvp in LocalTranspositionTableBuffer.Values)
+            {
+                dump_to.Add(kvp.Key, kvp.Value);
+            }
+        }
+
+        #endregion
 
 
         #region "Toolkit"
