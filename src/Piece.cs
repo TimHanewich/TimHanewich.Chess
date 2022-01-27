a75714d496751fac8f3630da5ddd603279323e28
Tim Hanewich <tahanewich@live.com>
2022-01-26 20:34:20 -0500

    Changed the way potential knight moves are delivered

--- src/Piece.cs
+++ src/Piece.cs
@@ -3228,72 +3228,267 @@ namespace TimHanewich.Chess
     
         //Returns the potential target squares for a knight in a partiuclar square
         private Position[] PotentialKnightMoves(Position p)
-        {
-            Dictionary<Position, Position[]> dict = new Dictionary<Position, Position[]>();
-            dict.Add(Position.A1, new Position[]{Position.B3,Position.C2});dict.Add(Position.A2, new Position[]{Position.B4,Position.C3,Position.C1});
-            dict.Add(Position.A3, new Position[]{Position.B5,Position.C4,Position.C2,Position.B1});
-            dict.Add(Position.A4, new Position[]{Position.B6,Position.C5,Position.C3,Position.B2});
-            dict.Add(Position.A5, new Position[]{Position.B7,Position.C6,Position.C4,Position.B3});
-            dict.Add(Position.A6, new Position[]{Position.B8,Position.C7,Position.C5,Position.B4});
-            dict.Add(Position.A7, new Position[]{Position.C8,Position.C6,Position.B5});
-            dict.Add(Position.A8, new Position[]{Position.C7,Position.B6});
-            dict.Add(Position.B1, new Position[]{Position.A3,Position.C3,Position.D2});
-            dict.Add(Position.B2, new Position[]{Position.A4,Position.C4,Position.D3,Position.D1});
-            dict.Add(Position.B3, new Position[]{Position.A5,Position.C5,Position.D4,Position.D2,Position.A1,Position.C1});
-            dict.Add(Position.B4, new Position[]{Position.A6,Position.C6,Position.D5,Position.D3,Position.A2,Position.C2});
-            dict.Add(Position.B5, new Position[]{Position.A7,Position.C7,Position.D6,Position.D4,Position.A3,Position.C3});
-            dict.Add(Position.B6, new Position[]{Position.A8,Position.C8,Position.D7,Position.D5,Position.A4,Position.C4});
-            dict.Add(Position.B7, new Position[]{Position.D8,Position.D6,Position.A5,Position.C5});
-            dict.Add(Position.B8, new Position[]{Position.D7,Position.A6,Position.C6});
-            dict.Add(Position.C1, new Position[]{Position.B3,Position.D3,Position.A2,Position.E2});
-            dict.Add(Position.C2, new Position[]{Position.B4,Position.D4,Position.A3,Position.E3,Position.A1,Position.E1});
-            dict.Add(Position.C3, new Position[]{Position.B5,Position.D5,Position.A4,Position.E4,Position.A2,Position.E2,Position.B1,Position.D1});
-            dict.Add(Position.C4, new Position[]{Position.B6,Position.D6,Position.A5,Position.E5,Position.A3,Position.E3,Position.B2,Position.D2});
-            dict.Add(Position.C5, new Position[]{Position.B7,Position.D7,Position.A6,Position.E6,Position.A4,Position.E4,Position.B3,Position.D3});
-            dict.Add(Position.C6, new Position[]{Position.B8,Position.D8,Position.A7,Position.E7,Position.A5,Position.E5,Position.B4,Position.D4});
-            dict.Add(Position.C7, new Position[]{Position.A8,Position.E8,Position.A6,Position.E6,Position.B5,Position.D5});
-            dict.Add(Position.C8, new Position[]{Position.A7,Position.E7,Position.B6,Position.D6});
-            dict.Add(Position.D1, new Position[]{Position.C3,Position.E3,Position.B2,Position.F2});
-            dict.Add(Position.D2, new Position[]{Position.C4,Position.E4,Position.B3,Position.F3,Position.B1,Position.F1});
-            dict.Add(Position.D3, new Position[]{Position.C5,Position.E5,Position.B4,Position.F4,Position.B2,Position.F2,Position.C1,Position.E1});
-            dict.Add(Position.D4, new Position[]{Position.C6,Position.E6,Position.B5,Position.F5,Position.B3,Position.F3,Position.C2,Position.E2});
-            dict.Add(Position.D5, new Position[]{Position.C7,Position.E7,Position.B6,Position.F6,Position.B4,Position.F4,Position.C3,Position.E3});
-            dict.Add(Position.D6, new Position[]{Position.C8,Position.E8,Position.B7,Position.F7,Position.B5,Position.F5,Position.C4,Position.E4});
-            dict.Add(Position.D7, new Position[]{Position.B8,Position.F8,Position.B6,Position.F6,Position.C5,Position.E5});
-            dict.Add(Position.D8, new Position[]{Position.B7,Position.F7,Position.C6,Position.E6});
-            dict.Add(Position.E1, new Position[]{Position.D3,Position.F3,Position.C2,Position.G2});
-            dict.Add(Position.E2, new Position[]{Position.D4,Position.F4,Position.C3,Position.G3,Position.C1,Position.G1});
-            dict.Add(Position.E3, new Position[]{Position.D5,Position.F5,Position.C4,Position.G4,Position.C2,Position.G2,Position.D1,Position.F1});
-            dict.Add(Position.E4, new Position[]{Position.D6,Position.F6,Position.C5,Position.G5,Position.C3,Position.G3,Position.D2,Position.F2});
-            dict.Add(Position.E5, new Position[]{Position.D7,Position.F7,Position.C6,Position.G6,Position.C4,Position.G4,Position.D3,Position.F3});
-            dict.Add(Position.E6, new Position[]{Position.D8,Position.F8,Position.C7,Position.G7,Position.C5,Position.G5,Position.D4,Position.F4});
-            dict.Add(Position.E7, new Position[]{Position.C8,Position.G8,Position.C6,Position.G6,Position.D5,Position.F5});
-            dict.Add(Position.E8, new Position[]{Position.C7,Position.G7,Position.D6,Position.F6});
-            dict.Add(Position.F1, new Position[]{Position.E3,Position.G3,Position.D2,Position.H2});
-            dict.Add(Position.F2, new Position[]{Position.E4,Position.G4,Position.D3,Position.H3,Position.D1,Position.H1});
-            dict.Add(Position.F3, new Position[]{Position.E5,Position.G5,Position.D4,Position.H4,Position.D2,Position.H2,Position.E1,Position.G1});
-            dict.Add(Position.F4, new Position[]{Position.E6,Position.G6,Position.D5,Position.H5,Position.D3,Position.H3,Position.E2,Position.G2});
-            dict.Add(Position.F5, new Position[]{Position.E7,Position.G7,Position.D6,Position.H6,Position.D4,Position.H4,Position.E3,Position.G3});
-            dict.Add(Position.F6, new Position[]{Position.E8,Position.G8,Position.D7,Position.H7,Position.D5,Position.H5,Position.E4,Position.G4});
-            dict.Add(Position.F7, new Position[]{Position.D8,Position.H8,Position.D6,Position.H6,Position.E5,Position.G5});
-            dict.Add(Position.F8, new Position[]{Position.D7,Position.H7,Position.E6,Position.G6});
-            dict.Add(Position.G1, new Position[]{Position.F3,Position.H3,Position.E2});
-            dict.Add(Position.G2, new Position[]{Position.F4,Position.H4,Position.E3,Position.E1});
-            dict.Add(Position.G3, new Position[]{Position.F5,Position.H5,Position.E4,Position.E2,Position.F1,Position.H1});
-            dict.Add(Position.G4, new Position[]{Position.F6,Position.H6,Position.E5,Position.E3,Position.F2,Position.H2});
-            dict.Add(Position.G5, new Position[]{Position.F7,Position.H7,Position.E6,Position.E4,Position.F3,Position.H3});
-            dict.Add(Position.G6, new Position[]{Position.F8,Position.H8,Position.E7,Position.E5,Position.F4,Position.H4});
-            dict.Add(Position.G7, new Position[]{Position.E8,Position.E6,Position.F5,Position.H5});
-            dict.Add(Position.G8, new Position[]{Position.E7,Position.F6,Position.H6});
-            dict.Add(Position.H1, new Position[]{Position.G3,Position.F2});
-            dict.Add(Position.H2, new Position[]{Position.G4,Position.F3,Position.F1});
-            dict.Add(Position.H3, new Position[]{Position.G5,Position.F4,Position.F2,Position.G1});
-            dict.Add(Position.H4, new Position[]{Position.G6,Position.F5,Position.F3,Position.G2});
-            dict.Add(Position.H5, new Position[]{Position.G7,Position.F6,Position.F4,Position.G3});
-            dict.Add(Position.H6, new Position[]{Position.G8,Position.F7,Position.F5,Position.G4});
-            dict.Add(Position.H7, new Position[]{Position.F8,Position.F6,Position.G5});
-            dict.Add(Position.H8, new Position[]{Position.F7,Position.G6});
-            return dict[p];
+        {     
+            if (p == Position.A1)
+            {
+                    return new Position[]{Position.B3,Position.C2};
+            }
+            else if (p == Position.A2)
+            {
+                    return new Position[]{Position.B4,Position.C3,Position.C1};
+            }
+            else if (p == Position.A3)
+            {
+                    return new Position[]{Position.B5,Position.C4,Position.C2,Position.B1};
+            }
+            else if (p == Position.A4)
+            {
+                    return new Position[]{Position.B6,Position.C5,Position.C3,Position.B2};
+            }
+            else if (p == Position.A5)
+            {
+                    return new Position[]{Position.B7,Position.C6,Position.C4,Position.B3};
+            }
+            else if (p == Position.A6)
+            {
+                    return new Position[]{Position.B8,Position.C7,Position.C5,Position.B4};
+            }
+            else if (p == Position.A7)
+            {
+                    return new Position[]{Position.C8,Position.C6,Position.B5};
+            }
+            else if (p == Position.A8)
+            {
+                    return new Position[]{Position.C7,Position.B6};
+            }
+            else if (p == Position.B1)
+            {
+                    return new Position[]{Position.A3,Position.C3,Position.D2};
+            }
+            else if (p == Position.B2)
+            {
+                    return new Position[]{Position.A4,Position.C4,Position.D3,Position.D1};
+            }
+            else if (p == Position.B3)
+            {
+                    return new Position[]{Position.A5,Position.C5,Position.D4,Position.D2,Position.A1,Position.C1};
+            }
+            else if (p == Position.B4)
+            {
+                    return new Position[]{Position.A6,Position.C6,Position.D5,Position.D3,Position.A2,Position.C2};
+            }
+            else if (p == Position.B5)
+            {
+                    return new Position[]{Position.A7,Position.C7,Position.D6,Position.D4,Position.A3,Position.C3};
+            }
+            else if (p == Position.B6)
+            {
+                    return new Position[]{Position.A8,Position.C8,Position.D7,Position.D5,Position.A4,Position.C4};
+            }
+            else if (p == Position.B7)
+            {
+                    return new Position[]{Position.D8,Position.D6,Position.A5,Position.C5};
+            }
+            else if (p == Position.B8)
+            {
+                    return new Position[]{Position.D7,Position.A6,Position.C6};
+            }
+            else if (p == Position.C1)
+            {
+                    return new Position[]{Position.B3,Position.D3,Position.A2,Position.E2};
+            }
+            else if (p == Position.C2)
+            {
+                    return new Position[]{Position.B4,Position.D4,Position.A3,Position.E3,Position.A1,Position.E1};
+            }
+            else if (p == Position.C3)
+            {
+                    return new Position[]{Position.B5,Position.D5,Position.A4,Position.E4,Position.A2,Position.E2,Position.B1,Position.D1};
+            }
+            else if (p == Position.C4)
+            {
+                    return new Position[]{Position.B6,Position.D6,Position.A5,Position.E5,Position.A3,Position.E3,Position.B2,Position.D2};
+            }
+            else if (p == Position.C5)
+            {
+                    return new Position[]{Position.B7,Position.D7,Position.A6,Position.E6,Position.A4,Position.E4,Position.B3,Position.D3};
+            }
+            else if (p == Position.C6)
+            {
+                    return new Position[]{Position.B8,Position.D8,Position.A7,Position.E7,Position.A5,Position.E5,Position.B4,Position.D4};
+            }
+            else if (p == Position.C7)
+            {
+                    return new Position[]{Position.A8,Position.E8,Position.A6,Position.E6,Position.B5,Position.D5};
+            }
+            else if (p == Position.C8)
+            {
+                    return new Position[]{Position.A7,Position.E7,Position.B6,Position.D6};
+            }
+            else if (p == Position.D1)
+            {
+                    return new Position[]{Position.C3,Position.E3,Position.B2,Position.F2};
+            }
+            else if (p == Position.D2)
+            {
+                    return new Position[]{Position.C4,Position.E4,Position.B3,Position.F3,Position.B1,Position.F1};
+            }
+            else if (p == Position.D3)
+            {
+                    return new Position[]{Position.C5,Position.E5,Position.B4,Position.F4,Position.B2,Position.F2,Position.C1,Position.E1};
+            }
+            else if (p == Position.D4)
+            {
+                    return new Position[]{Position.C6,Position.E6,Position.B5,Position.F5,Position.B3,Position.F3,Position.C2,Position.E2};
+            }
+            else if (p == Position.D5)
+            {
+                    return new Position[]{Position.C7,Position.E7,Position.B6,Position.F6,Position.B4,Position.F4,Position.C3,Position.E3};
+            }
+            else if (p == Position.D6)
+            {
+                    return new Position[]{Position.C8,Position.E8,Position.B7,Position.F7,Position.B5,Position.F5,Position.C4,Position.E4};
+            }
+            else if (p == Position.D7)
+            {
+                    return new Position[]{Position.B8,Position.F8,Position.B6,Position.F6,Position.C5,Position.E5};
+            }
+            else if (p == Position.D8)
+            {
+                    return new Position[]{Position.B7,Position.F7,Position.C6,Position.E6};
+            }
+            else if (p == Position.E1)
+            {
+                    return new Position[]{Position.D3,Position.F3,Position.C2,Position.G2};
+            }
+            else if (p == Position.E2)
+            {
+                    return new Position[]{Position.D4,Position.F4,Position.C3,Position.G3,Position.C1,Position.G1};
+            }
+            else if (p == Position.E3)
+            {
+                    return new Position[]{Position.D5,Position.F5,Position.C4,Position.G4,Position.C2,Position.G2,Position.D1,Position.F1};
+            }
+            else if (p == Position.E4)
+            {
+                    return new Position[]{Position.D6,Position.F6,Position.C5,Position.G5,Position.C3,Position.G3,Position.D2,Position.F2};
+            }
+            else if (p == Position.E5)
+            {
+                    return new Position[]{Position.D7,Position.F7,Position.C6,Position.G6,Position.C4,Position.G4,Position.D3,Position.F3};
+            }
+            else if (p == Position.E6)
+            {
+                    return new Position[]{Position.D8,Position.F8,Position.C7,Position.G7,Position.C5,Position.G5,Position.D4,Position.F4};
+            }
+            else if (p == Position.E7)
+            {
+                    return new Position[]{Position.C8,Position.G8,Position.C6,Position.G6,Position.D5,Position.F5};
+            }
+            else if (p == Position.E8)
+            {
+                    return new Position[]{Position.C7,Position.G7,Position.D6,Position.F6};
+            }
+            else if (p == Position.F1)
+            {
+                    return new Position[]{Position.E3,Position.G3,Position.D2,Position.H2};
+            }
+            else if (p == Position.F2)
+            {
+                    return new Position[]{Position.E4,Position.G4,Position.D3,Position.H3,Position.D1,Position.H1};
+            }
+            else if (p == Position.F3)
+            {
+                    return new Position[]{Position.E5,Position.G5,Position.D4,Position.H4,Position.D2,Position.H2,Position.E1,Position.G1};
+            }
+            else if (p == Position.F4)
+            {
+                    return new Position[]{Position.E6,Position.G6,Position.D5,Position.H5,Position.D3,Position.H3,Position.E2,Position.G2};
+            }
+            else if (p == Position.F5)
+            {
+                    return new Position[]{Position.E7,Position.G7,Position.D6,Position.H6,Position.D4,Position.H4,Position.E3,Position.G3};
+            }
+            else if (p == Position.F6)
+            {
+                    return new Position[]{Position.E8,Position.G8,Position.D7,Position.H7,Position.D5,Position.H5,Position.E4,Position.G4};
+            }
+            else if (p == Position.F7)
+            {
+                    return new Position[]{Position.D8,Position.H8,Position.D6,Position.H6,Position.E5,Position.G5};
+            }
+            else if (p == Position.F8)
+            {
+                    return new Position[]{Position.D7,Position.H7,Position.E6,Position.G6};
+            }
+            else if (p == Position.G1)
+            {
+                    return new Position[]{Position.F3,Position.H3,Position.E2};
+            }
+            else if (p == Position.G2)
+            {
+                    return new Position[]{Position.F4,Position.H4,Position.E3,Position.E1};
+            }
+            else if (p == Position.G3)
+            {
+                    return new Position[]{Position.F5,Position.H5,Position.E4,Position.E2,Position.F1,Position.H1};
+            }
+            else if (p == Position.G4)
+            {
+                    return new Position[]{Position.F6,Position.H6,Position.E5,Position.E3,Position.F2,Position.H2};
+            }
+            else if (p == Position.G5)
+            {
+                    return new Position[]{Position.F7,Position.H7,Position.E6,Position.E4,Position.F3,Position.H3};
+            }
+            else if (p == Position.G6)
+            {
+                    return new Position[]{Position.F8,Position.H8,Position.E7,Position.E5,Position.F4,Position.H4};
+            }
+            else if (p == Position.G7)
+            {
+                    return new Position[]{Position.E8,Position.E6,Position.F5,Position.H5};
+            }
+            else if (p == Position.G8)
+            {
+                    return new Position[]{Position.E7,Position.F6,Position.H6};
+            }
+            else if (p == Position.H1)
+            {
+                    return new Position[]{Position.G3,Position.F2};
+            }
+            else if (p == Position.H2)
+            {
+                    return new Position[]{Position.G4,Position.F3,Position.F1};
+            }
+            else if (p == Position.H3)
+            {
+                    return new Position[]{Position.G5,Position.F4,Position.F2,Position.G1};
+            }
+            else if (p == Position.H4)
+            {
+                    return new Position[]{Position.G6,Position.F5,Position.F3,Position.G2};
+            }
+            else if (p == Position.H5)
+            {
+                    return new Position[]{Position.G7,Position.F6,Position.F4,Position.G3};
+            }
+            else if (p == Position.H6)
+            {
+                    return new Position[]{Position.G8,Position.F7,Position.F5,Position.G4};
+            }
+            else if (p == Position.H7)
+            {
+                    return new Position[]{Position.F8,Position.F6,Position.G5};
+            }
+            else if (p == Position.H8)
+            {
+                    return new Position[]{Position.F7,Position.G6};
+            }
+            else
+            {
+                throw new Exception("An invalid position was provided to the Knight move generation");
+            }
         }
 
     }
